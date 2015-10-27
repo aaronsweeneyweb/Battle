1b9f6a196e49b20ffe76e30808e6ecdc873fe410
Aaron Sweeney <aaron.sweeney@ucdconnect.ie>
2015-10-27 15:48:18 +0000

    Feature test for entering names written

---
+++ spec/features/enter_names_spec.rb
@@ -0,0 +1,11 @@
+require 'capybara/rspec'
+
+feature 'Enter names' do
+  scenario 'submitting names' do
+    visit('/')
+    fill_in(:player_one_name, with: 'Sam')
+    fill_in(:player_two_name, with: 'Aaron')
+    click_button('Submit')
+    expect(page).to have_content 'Sam vs Aaron'
+  end
+end
